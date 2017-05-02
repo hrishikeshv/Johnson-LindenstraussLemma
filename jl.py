7b5aeae7e0a032d6224393f081c286b214e5947d
tapan <cs13b1030@iith.ac.in>
2017-05-03 03:31:39 +0530

    cauchy graphs

--- jl.py
+++ jl.py
@@ -6,7 +6,7 @@ from scipy.linalg import circulant
 from scipy.linalg import hadamard
 from scipy import stats
 
-def randomSubspace(subspaceDimension, ambientDimension, method="hadamard"):
+def randomSubspace(subspaceDimension, ambientDimension, method="cauchy"):
     if method == "gaussian":
         return np.random.normal(0, 1, size=(subspaceDimension, ambientDimension))
     elif method == "circulant":
@@ -16,29 +16,28 @@ def randomSubspace(subspaceDimension, ambientDimension, method="hadamard"):
         return cmatrix.dot(dmatrix)
     elif method == "sparse":
         custm = stats.rv_discrete(values=([-1,0,1],[1.0/6,2.0/3,1.0/6]))
-        unnorm_matrix = custm.rvs(size=(subspaceDimension, ambientDimension))
-        return unnorm_matrix
+        return math.sqrt(3)*custm.rvs(size=(subspaceDimension, ambientDimension))
     elif method == "hadamard":
         P = np.random.normal(0, 1, size=(subspaceDimension, ambientDimension))
         H = hadamard(ambientDimension)
         custm = stats.rv_discrete(values=([-1,1],[1.0/2, 1.0/2]))
         D = np.diag(custm.rvs(size=ambientDimension))
-        unnorm_matrix = P.dot(H.dot(D))
-        # print(unnorm_matrix[0])
-        return unnorm_matrix / math.sqrt(ambientDimension)
-        # unnorm_matrix =  P.dot(H.dot(D))
-        # l2norm = np.sqrt((unnorm_matrix * unnorm_matrix).sum(axis=1))
-        # ret_matrix = unnorm_matrix / l2norm.reshape(unnorm_matrix.shape[0],1)
-        # print(ret_matrix[0])
-        # return ret_matrix
+        return (1/math.sqrt(ambientDimension))*P.dot(H.dot(D))
+    elif method == "cauchy":
+    	return np.random.standard_cauchy((subspaceDim, ambientDimension))/math.sqrt(ambientDimension)
+
 
 def checkTheorem(oldData, newData, epsilon):
     numBadPoints = 0
     count=0
     error = []
     for (x,y), (x2,y2) in zip(combinations(oldData, 2), combinations(newData, 2)):
-        oldNorm = np.linalg.norm(x2-y2)**2 
-        newNorm = np.linalg.norm(x-y)**2 
+
+        #oldNorm = np.linalg.norm(x2-y2, 1) for cauchy distribution
+        #newNorm = np.linalg.norm(x-y, 1)
+
+        oldNorm = np.linalg.norm(x2-y2) ** 2
+        newNorm = np.linalg.norm(x-y) ** 2
 
         if newNorm == 0 or oldNorm == 0:
             continue
@@ -59,7 +58,7 @@ def jl(data, subspaceDim):
     return transformed
 
 if __name__ == '__main__':
-    size = 1000
+    size = 4000
     n = 4096
     eps = 0.25
     method = "gaussian"
@@ -72,7 +71,6 @@ if __name__ == '__main__':
     elif method == "circulant":
         subspaceDim = int(math.ceil(2.0*(math.log(size))/eps**2))
 
-    data = np.random.uniform(-1,1,size=(size,n))
     if datatype == "dense":
         data = np.random.uniform(-1,1,size=(size,n))
     elif datatype == "sparse":
